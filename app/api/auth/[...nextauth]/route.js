9866ae83a855f12fe6dde93b65a565a0cc2abb9b
sudhanshu mishra <sudhanshumishra9876@gmail.com>
2024-01-11 22:07:27 +0530

    added

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -14,10 +14,4 @@ const handler = NextAuth({
   async signIn({ profile }) {},
 });
 
-export const generateStaticParams = () => {
-  return {
-    // Your additional static parameters
-  };
-};
-
 export { handler as GET, handler as POST };
--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: "export", basePath: "/chat-system" };
+const nextConfig = {};
 
 module.exports = nextConfig;
